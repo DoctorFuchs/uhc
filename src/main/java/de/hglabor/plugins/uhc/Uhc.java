2043b5fafb7c82329f669e176ee83b71f81014c2
BestAutoyes <enrico-eifert@t-online.de>
2021-02-26 23:33:42 +0100

    Registered new scenarios

--- src/main/java/de/hglabor/plugins/uhc/Uhc.java
+++ src/main/java/de/hglabor/plugins/uhc/Uhc.java
@@ -3,8 +3,7 @@ package de.hglabor.plugins.uhc;
 import de.hglabor.plugins.uhc.game.GameManager;
 import de.hglabor.plugins.uhc.game.command.StartCommand;
 import de.hglabor.plugins.uhc.game.config.UHCConfig;
-import de.hglabor.plugins.uhc.game.scenarios.Netherless;
-import de.hglabor.plugins.uhc.game.scenarios.Teams;
+import de.hglabor.plugins.uhc.game.scenarios.*;
 import dev.jorel.commandapi.CommandAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.WorldCreator;
@@ -22,8 +21,15 @@ public final class Uhc extends JavaPlugin {
         Bukkit.createWorld(new WorldCreator("lobby"));
 
         GameManager gameManager = GameManager.INSTANCE;
+        gameManager.addScenario(BloodDiamondsNetherite.INSTANCE);
+        gameManager.addScenario(CrossBowless.INSTANCE);
+        gameManager.addScenario(CutClean.INSTANCE);
+        gameManager.addScenario(Fireless.INSTANCE);
+        gameManager.addScenario(HasteyBoys.INSTANCE);
         gameManager.addScenario(Netherless.INSTANCE);
+        gameManager.addScenario(RodKnockback.INSTANCE);
         gameManager.addScenario(Teams.INSTANCE);
+        gameManager.addScenario(Timebomb.INSTANCE);
         gameManager.run();
 
         CommandAPI.onEnable(this);
