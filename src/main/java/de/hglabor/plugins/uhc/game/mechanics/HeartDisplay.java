bf27ee59ba531b1833218b99a8a2f6c44cb749c2
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-02-28 15:42:20 +0100

    Added Arrow Heart Display

--- src/main/java/de/hglabor/plugins/uhc/game/mechanics/HeartDisplay.java
+++ src/main/java/de/hglabor/plugins/uhc/game/mechanics/HeartDisplay.java
@@ -3,11 +3,15 @@ package de.hglabor.plugins.uhc.game.mechanics;
 import de.hglabor.plugins.uhc.player.PlayerList;
 import de.hglabor.plugins.uhc.player.UHCPlayer;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.AbstractArrow;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.projectiles.ProjectileSource;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.RenderType;
@@ -29,6 +33,21 @@ public class HeartDisplay implements Listener {
     public void onEntityRegainHealth(EntityRegainHealthEvent event) {
         if (event.getEntity() instanceof Player) {
             updateHeal((Player) event.getEntity());
+        }
+    }
+
+    @EventHandler
+    public void onProjectileHit(ProjectileHitEvent event) {
+        if (event.getHitEntity() == null) {
+            return;
+        }
+        if (!(event.getEntity() instanceof AbstractArrow)) {
+            return;
+        }
+        ProjectileSource shooter = event.getEntity().getShooter();
+        if (shooter instanceof Player) {
+            LivingEntity hitEntity = (LivingEntity) event.getHitEntity();
+            ((Player) shooter).sendMessage(ChatColor.AQUA + hitEntity.getName() + " is now at " + ChatColor.GREEN + hitEntity.getHealth() + ChatColor.RED + " ❤");
         }
     }
 
